566de7740825448381a96cf6efe8af219d0348d5
Brätz, Paul <paul.braetz@rhomicro.com>
2023-08-28 19:31:07 +0200

    added interface field tests

---
+++ Tests/Parent/FieldTests.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroRecords.Tests.Parent
+{
+    [TestClass]
+    public class FieldTests
+    {
+        private static Object[][] Data
+        {
+            get
+            {
+                return new Object[][]
+                {
+                    new Object[]
+                    {
+                        """
+                        using RhoMicro.MacroRecords;
+                        [MacroRecord]
+                        [Field(typeof(System.IComparable), "Field1")]
+                        partial class TVO
+                        {
+                        }
+                        """,
+                        """
+                        public readonly System.IComparable Field1;
+                        """
+                    },
+                    new Object[]
+                    {
+                        """
+                        using RhoMicro.MacroRecords;
+                        [MacroRecord,
+                        Field(typeof(int), "Field1"),
+                        Field(typeof(System.IComparable), "Field2")]
+                        partial class TVO
+                        {
+                        }
+                        """,
+                        """
+                        public readonly System.Int32 Field1;
+                        public readonly System.IComparable Field2;
+                        """
+                    },
+                    new Object[]
+                    {
+                        """
+                        using RhoMicro.MacroRecords;
+                        [MacroRecord,
+                        Field(typeof(int), "Field1"),
+                        Field(typeof(System.String), "Field2")]
+                        partial class TVO
+                        {
+                        }
+                        """,
+                        """
+                        public readonly System.Int32 Field1;
+                        public readonly System.String Field2;
+                        """
+                    },
+                    new Object[]
+                    {
+                        """
+                        using RhoMicro.MacroRecords;
+                        [MacroRecord]
+                        partial class TVO
+                        {
+                        }
+                        """,
+                        String.Empty
+                    },
+                    new Object[]
+                    {
+                        """
+                        using RhoMicro.MacroRecords;
+                        [MacroRecord,
+                        Field(typeof(object), "ObjectField"),
+                        Field(typeof(string), "StringField")]
+                        partial class TVO
+                        {
+                        }
+                        """,
+                        """
+                        public readonly System.Object ObjectField;
+                        public readonly System.String StringField;
+                        """
+                    }
+                };
+            }
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(Data))]
+        public void GeneratesConstructorCorrectly(String consumer, String expected)
+        {
+            //Arrange
+            var builder = Util.CreateBuilder(consumer);
+
+            //Act
+            var actual = builder.AddParentFields().BuildCore();
+
+            //Assert
+            Assertions.AreEquivalent(expected, actual);
+        }
+    }
+}
