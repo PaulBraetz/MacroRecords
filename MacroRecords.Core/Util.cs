47ac7a7149baf3cd1f14f616d21f2142f35b15ae
Brätz, Paul <paul.braetz@rhomicro.com>
2023-08-30 14:10:33 +0200

    refactored factories into Util

--- MacroRecords.Core/Util.cs
+++ MacroRecords.Core/Util.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using RhoMicro.CodeAnalysis;
+using RhoMicro.CodeAnalysis.Attributes;
+
+using System;
 using System.Collections.Generic;
 
 namespace RhoMicro.MacroRecords.Core
 {
     internal static class Util
     {
+
+        public static readonly IAttributeFactory<FieldAttribute> FieldAttributeFactory = AttributeFactory<FieldAttribute>.Create();
+        public static readonly IAttributeFactory<MacroRecordAttribute> MacroRecordAttributeFactory = AttributeFactory<MacroRecordAttribute>.Create();
+
+        public static readonly ITypeIdentifier FieldAttributeIdentifier = TypeIdentifier.Create<FieldAttribute>();
+        public static readonly ITypeIdentifier MacroRecordAttributeIdentifier = TypeIdentifier.Create<MacroRecordAttribute>();
+
         private static readonly IReadOnlyDictionary<Int32, String> _visibilities =
             new Dictionary<Int32, String>()
             {
